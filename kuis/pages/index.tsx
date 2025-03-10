2c7e9a7f28b57ebd36e46051fdac58d789d2a92d
yoe174 <yoechannelpjr@gmail.com>
2025-03-10 10:27:23 +0700

    first commit langkah 1

---
+++ kuis/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Selamat Datang di Website Saya!</h1>
+      <p>Ini adalah halaman Utama.</p>
+    </div>
+  );
+};
+
+export default HomePage;
