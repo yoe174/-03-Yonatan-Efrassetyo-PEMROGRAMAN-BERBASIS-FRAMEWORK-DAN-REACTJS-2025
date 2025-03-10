5f81375cefa4e8a3defb3c6b9fc4c6d099584ae5
yoe174 <yoechannelpjr@gmail.com>
2025-03-10 11:20:08 +0700

    langkah 6

---
+++ kuis/pages/about.js
@@ -0,0 +1,10 @@
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>Tentang Kami</h1>
+            <p>Kami adalah perusahaan yang mengkhususkan diri dalam  pembuatan website berkualitas tinggi</p>
+        </div>
+    );
+};
+
+export default AboutPage;
--- kuis/pages/index.tsx
+++ kuis/pages/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Link from "next/link";
 
 const HomePage = () => {
   return (
     <div>
       <h1>Selamat Datang di Website Saya!</h1>
       <p>Ini adalah halaman Utama.</p>
+      <Link href={"/about"}>
+        Tentang Kami
+      </Link>
     </div>
   );
 };
